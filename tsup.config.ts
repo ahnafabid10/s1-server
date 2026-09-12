f08f19b0a614ff4dcf0500ae6dc7c9235d968dd4
Ahnaf Abid <ahnafabid600@gmail.com>
2026-09-12 16:11:16 +0600

    build: bundle @prisma/client and ignore vercel build directory

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,19 +2,18 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     entry: ["src/server.ts"],
-    format: ["esm"], // Keep this as ESM
+    format: ["esm"],
     target: "esnext",
     outDir: "dist",
     clean: true,
     bundle: true,
     splitting: false,
     sourcemap: true,
-    // Add this banner to shim require() for CJS dependencies
-
+    noExternal: ["@prisma/client"],
     banner: {
         js: `
-   import { createRequire } from 'module';
-   const require = createRequire(import.meta.url);
-  `,
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+`,
     },
 });
